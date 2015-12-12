612d9f23ccefd92d8a999345b6717858b86bc9a5
A.J. Venter <ajventer@gmail.com>
2015-12-12 09:15:24 +0200

    Mapview object coming together

--- src/usr/share/mirthless/lib/graphics.py
+++ src/usr/share/mirthless/lib/graphics.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import pygame
 from pygame.locals import *
+from util import debug
 
 
 def get_screen(resx, resy, hardware, fullscreen):
@@ -22,10 +23,14 @@ def scrn_print(surface, text, x, y, size=32, color=(0,0,0)):
     surface.blit(text, textpos)
 
 class Tilemap(object):
-    def __init__(self, filename, width, height):
+    def __init__(self, filename, width, height, scale=False):
         #Credit - this came almost directly from the tutorial at: https://qq.readthedocs.org/en/latest/tiles.html
+
         image = pygame.image.load(filename).convert()
         image_width, image_height = image.get_size()
+        if scale:
+            image_width = image_width *2
+            image_height = image_height *2
         tile_table = []
         for tile_x in range(0, image_width/width):
             line = []
@@ -34,9 +39,42 @@ class Tilemap(object):
                 rect = (tile_x*width, tile_y*height, width, height)
                 line.append(image.subsurface(rect))
         self.tile_table = tile_table
+        self.scale = scale
+        self.x = tile_x
+        self.y = tile_y
+        self.w = width
+        self.h = height
+
+
+    def size(self):
+        return (self.x, self.y)
 
     def table(self):
         return self.tile_table
 
     def tile(self, x, y):
+        if self.scale:
+            image = self.tile_table[x][y]
+            image = pygame.transform.scale2x(image)
+            return image
         return self.tile_table[x][y]
+
+class Mapview(object):
+    def __init__(self, tilemap, w, h):
+        self.tilemap = tilemap
+        self.surface = pygame.Surface((w * tilemap.w, h * tilemap.h))
+        debug(self.surface)
+
+    def set_bg(self, x, y, tx, ty):
+        pos_x = x * self.tilemap.w
+        pos_y = y * self.tilemap.h
+        debug ('Blitting %sx%s to position %sx%s' % (tx,ty,pos_x,pos_y))
+        self.surface.blit(self.tilemap.tile(tx, ty), (pos_x, pos_y))
+
+
+
+
+
+
+
+
