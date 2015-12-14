05ba8fc225659f4e42e6a4580c5eafa0d966b38b
A.J. Venter <ajventer@gmail.com>
2015-12-14 23:09:48 +0200

    Map-click inputs working as advertised. Too many sprites in the previous design

--- src/usr/share/mirthless/lib/mapview.py
+++ src/usr/share/mirthless/lib/mapview.py
@@ -16,6 +16,23 @@ class Mapview(object):
         self.frontend.eventstack.register_event("button1", self, self.click)
         self.image  = pygame.Surface((size, size))
 
+
+    def tileimage(self, x, y, scale):
+        tileimage = pygame.Surface((self.tilesize, self.tilesize))
+        backgroundpath = tile.background()
+        if backgroundpath:
+            tileimage = self.frontend.tilemaps.get_by_path(backgroundpath)
+            tileimage = pygame.transform.smoothscale(backgroundimage, (scale, scale))
+        if backgroundpath and tile.revealed():
+            self.image.blit(backgroundimage,(0,0))
+        else:
+            if self.frontend.mode == 'editor':
+                tileimage.fill((randrange(0,255),0,0,0))
+                tileimage.blit(render_text('%sX%s' %(x,y), size=16, color=(0,255,0)), (1,1))
+            else:
+                tileimage.fill((0,0,0,0))
+        return tileimage
+
     def loadmap(self, data):
         gamemap = GameMap(data)
         gamemap.initialize()
@@ -24,20 +41,7 @@ class Mapview(object):
                 tile = gamemap.tile(x,y)
                 scn_x = 50+(self.tilesize*x)
                 scn_y = 65+(self.tilesize*y)
-                tileimage = pygame.Surface((self.tilesize, self.tilesize))
-                backgroundpath = tile.background()
-                if backgroundpath:
-                    tileimage = self.frontend.tilemaps.get_by_path(backgroundpath)
-                    tileimage = pygame.transform.smoothscale(backgroundimage, (self.tilesize, self.tilesize))
-                if backgroundpath and tile.revealed():
-                    self.image.blit(backgroundimage,(0,0))
-                else:
-                    if self.frontend.mode == 'editor':
-                        tileimage.fill((randrange(0,255),0,0,0))
-                        tileimage.blit(render_text('%sX%s' %(x,y), size=16, color=(0,255,0)), (1,1))
-                    else:
-                        tileimage.fill((0,0,0,0))                
-                self.image.blit(tileimage,(self.tilesize*x, self.tilesize*y))
+                self.image.blit(tileimage(x,y, self.tilesize),(self.tilesize*x, self.tilesize*y))
 
     def click(self, pos):
         x, y = pos
@@ -45,6 +49,9 @@ class Mapview(object):
         y = y - 65
         map_x = int(x / self.tilesize)
         map_y = int(y / self.tilesize)
+
+        zoomimage = tileimage(map_x, map_y, 128)
+        self.frontend.sprites['rightwindow'].image.blit(zoomimage, (15,15))
         if self.frontend.mode == 'editor':
             messages.message('Editor click: %sx%s' % (map_x, map_y))
             return 
