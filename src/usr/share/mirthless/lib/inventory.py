5037b2ce66413678b85a920e3b8c813297e9e9b1
A.J. Venter <ajventer@gmail.com>
2015-12-20 04:02:04 +0200

    Started working on unequip

---
+++ src/usr/share/mirthless/lib/inventory.py
@@ -0,0 +1,83 @@
+import pygame
+from pygame.locals import *
+from item import Item
+from dialog import FloatDialog, ContainerDialog
+from util import file_list, debug, load_yaml, make_hash
+from animatedsprite import ButtonSprite
+import os
+
+class Inventory(FloatDialog):
+    def __init__(self, rect, frontend, char, layer=5, onclose=None):
+        self._layer = layer
+        FloatDialog.__init__(self, rect, frontend, layer)
+        self.rect = rect
+        self.frontend = frontend
+        self.npc = char.character_type == 'npc'
+        self.char = char
+        self.onclose = onclose
+        self.layout()
+
+    def layout(self):
+        self._rmtemp()
+        itemlist = []
+        for itemfile in file_list('items'):
+            itemfile = os.path.basename(itemfile)
+            itemlist.append(Item(load_yaml('items',itemfile)))
+        inventory_rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.w - 523, self.rect.h)
+        pack = ContainerDialog(
+            inventory_rect,
+            self.frontend,
+            'Pack:',
+            layer=self._layer +1,
+            items=[Item(i) for i in self.char.get('inventory/pack',[])],
+            onclose=self.update_inventory,
+            onselect=self.equip_item,
+            onselect_parms=[],
+            can_add=False,
+            can_remove=False,
+            addfrom=itemlist
+            )
+        self._addtemp('Inventory_pack_dialog', pack)
+        image_x = self.rect.w - 522
+        image_y = 10
+        self.image.blit(self.frontend.imagecache['inventory_background.png'], (image_x, image_y))
+        rects = load_yaml('images','gui_rects.yaml')
+        portrait = self.frontend.imagecache[self.char.get('personal/portrait')]
+        portrait = pygame.transform.smoothscale(portrait, (256,256))
+        prect = rects['inventory_portrait']
+        self.image.blit(portrait,(image_x + prect['x'],image_y + prect['y']))
+        image_x += self.rect.x
+        image_y += self.rect.y
+        for itemtuple in self.char.inventory_generator(['equiped']):
+            debug(itemtuple)
+            item, slot = itemtuple[1], itemtuple[2]
+            irect = rects[slot]
+            irect = pygame.Rect(image_x + irect['x'], image_y + irect['y'], irect['w'], irect['h'])
+            debug(slot, irect)
+            sprite = ButtonSprite(
+                self.frontend.tilemaps,
+                irect,
+                self.frontend.eventstack,
+                onclick=self.unequip_item,
+                onclick_params=[item],
+                animations=item.getsubtree('animations'),
+                layer=self._layer + 2,
+                fps=5,
+                sendself=True
+                )
+            sprite.setanimation('view')
+            self._addtemp(make_hash(), sprite)
+
+    def update_inventory(self, items, *args):
+        self.char.put('inventory/pack', items)
+        self.onclose()
+
+    def equip_item(self, item):
+        self.char.equip_item(item)
+        self.layout()
+
+    def unequip_item(self, item, *args):
+        self.char.unequip_item(item)
+        self.layout()
+
+
